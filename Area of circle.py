1e82d15bfc05a5fa674d847181fcb3d19f73c3a7
Anvitha-N <68950645+Anvitha-N@users.noreply.github.com>
2020-09-16 14:03:46 +0530

    Add files via upload
    >code for area of circle
    >code to print extension of filename

---
+++ Area of circle.py
@@ -0,0 +1,6 @@
+#python program for Area of Circle
+
+from math import pi
+r=float(input("Enter the radius of the circle : "))
+A=pi*r**2    #radius of the circle
+print("The area of the circle with radius " + str(r) + " is: " + str(A)) 
